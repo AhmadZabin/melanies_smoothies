2b35c41e16e7f972c4f996c3db5bc0a88a3b3a88
AhmadZabin <ahmad_zabin@hotmail.com>
2025-02-19 12:15:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,9 @@ session = cnx.session()
 
 #my_dataframe = session.table("smoothies.public.fruit_options")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
+
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
@@ -43,7 +45,7 @@ if ingredients_list:  # this will remove the brackets
             smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
             #st.text(smoothiefroot_response.json())
             sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
-            st.stop()
+
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
