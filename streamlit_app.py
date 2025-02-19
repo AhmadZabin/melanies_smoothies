166acc076f9d84ed3aaf315c8703970c6057eb82
AhmadZabin <ahmad_zabin@hotmail.com>
2025-02-19 11:48:16 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,8 @@ if ingredients_list:  # this will remove the brackets
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json())
+    #st.text(smoothiefroot_response.json())
+    sf_df=st.dataframe(data=(smoothiefroot_response.json(),use_container_width=True)
 
     #st.write(my_insert_stmt)
     #st.stop()
